adf5ae1535443c63fc41b4fa1d156973bb27b700
Aldair-ld <frankaldair29@gmail.com>
2024-06-18 17:11:03 -0500

    Update fun-doxeo_falso.js

--- plugins/fun-doxeo_falso.js
+++ plugins/fun-doxeo_falso.js
@@ -2,33 +2,29 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {
   let user = global.db.data.users[m.sender];
 
   if (command === 'personaje') {
-    if (text.includes('|') && text.split('|').length === 3 && m.quoted && m.quoted.mtype === 'imageMessage') {
-      let [personaje, nombre, biografia] = text.split('|').map(v => v.trim());
-      let foto = m.quoted.url;
+    if (text.includes('|') && text.split('|').length === 2) {
+      let [personaje, biografia] = text.split('|').map(v => v.trim());
 
       user.personaje = personaje;
-      user.nombre = nombre;
       user.biografia = biografia;
-      user.foto = foto;
 
-      return conn.reply(m.chat, `Registro completado:\n\n🎭 *Personaje:* ${personaje}\n📛 *Nombre:* ${nombre}\n📝 *Biografía:* ${biografia}\n🖼️ *Foto:*`, m);
+      return conn.reply(m.chat, `Registro completado:\n\n🎭 *Personaje:* ${personaje}\n📝 *Biografía:* ${biografia}`, m);
     } else {
-      return conn.reply(m.chat, `Uso incorrecto del comando. Por favor, usa el formato:\n${usedPrefix}personaje personaje|nombre|biografía\nY responde a este comando con una foto de tu personaje.`, m);
+      return conn.reply(m.chat, `Uso incorrecto del comando. Por favor, usa el formato:\n${usedPrefix}personaje personaje|biografía`, m);
     }
   }
 
   if (command === 'mirol') {
-    if (!user.personaje || !user.nombre || !user.biografia || !user.foto) {
+    if (!user.personaje || !user.biografia) {
       return conn.reply(m.chat, `Aún no has registrado tu rol. Usa el comando ${usedPrefix}personaje para registrarte.`, m);
     }
 
     let mensaje = `
 🔍 *Información de tu Rol:*
-📛 *Nombre:* ${user.nombre}
 🎭 *Personaje:* ${user.personaje}
 📝 *Biografía:* ${user.biografia}`;
 
-    await conn.sendMessage(m.chat, { image: { url: user.foto }, caption: mensaje }, { quoted: m });
+    await conn.sendMessage(m.chat, mensaje, { quoted: m });
   }
 };
 
